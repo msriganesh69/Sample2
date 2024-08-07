0bab289b6a99765e50e523ac5965b94e64a7450c
msriganesh69 <msriganesh69@gmail.com>
2024-08-07 23:06:23 +0530

    new feature added

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -2,11 +2,14 @@ package com.sqisland.android.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    //adding feature
+    Log.d("Sample commit", "sample test");
   }
 }
